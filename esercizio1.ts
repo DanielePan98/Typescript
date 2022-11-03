94d20e7633c85d40f9b3105db6dfc649e955f64c
DanielePan98 <98751947+DanielePan98@users.noreply.github.com>
2022-11-03 12:14:27 +0100

    1 file added

---
+++ esercizio1.ts
@@ -0,0 +1,26 @@
+
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+function correttore(testoA:string,testoB:string){
+
+  if(testoA.includes('%')){
+    testoA='';
+    for(let i=testoA.length-1;i>=0;i--){
+      testoA+=testoA[i];
+    }
+  }
+  if(testoB.includes('%')){
+    testoB='';
+    for(let i=testoB.length-1;i>=0;i--){
+      testoB+=testoB[i];
+    }
+  }
+  return testoA + testoB;
+}
+
+console.log(correttore('tavo','o%nil'))
