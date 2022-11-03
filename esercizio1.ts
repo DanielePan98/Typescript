3406cac9eb53b10f7002bac17e95565e4bf94110
DanielePan98 <98751947+DanielePan98@users.noreply.github.com>
2022-11-03 12:14:50 +0100

    1 file modified

--- esercizio1.ts
+++ esercizio1.ts
@@ -1,4 +1,3 @@
-
 // Import stylesheets
 import './style.css';
 
@@ -6,21 +5,20 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-function correttore(testoA:string,testoB:string){
-
-  if(testoA.includes('%')){
-    testoA='';
-    for(let i=testoA.length-1;i>=0;i--){
-      testoA+=testoA[i];
+function correttore(testoA: string, testoB: string): string {
+  if (testoA.includes('%')) {
+    testoA = '';
+    for (let i = testoA.length - 1; i >= 0; i--) {
+      testoA += testoA[i];
     }
   }
-  if(testoB.includes('%')){
-    testoB='';
-    for(let i=testoB.length-1;i>=0;i--){
-      testoB+=testoB[i];
+  if (testoB.includes('%')) {
+    testoB = '';
+    for (let i = testoB.length - 1; i >= 0; i--) {
+      testoB += testoB[i];
     }
   }
   return testoA + testoB;
 }
 
-console.log(correttore('tavo','o%nil'))
+console.log(correttore('tavo', 'o%nil'));
